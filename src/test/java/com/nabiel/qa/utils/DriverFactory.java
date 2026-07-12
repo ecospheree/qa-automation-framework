f8bdcf0f029c8516abc5e5e1547ca56c0639be38
ecospheree <prayoganabiel74@gmail.com>
2026-07-12 17:47:04 +0700

    Run Selenium in headless mode

--- src/test/java/com/nabiel/qa/utils/DriverFactory.java
+++ src/test/java/com/nabiel/qa/utils/DriverFactory.java
@@ -3,23 +3,27 @@ package com.nabiel.qa.utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
     private static WebDriver driver;
 
     public static WebDriver getDriver() {
+
         if (driver == null) {
 
-            String browser = ConfigReader.getProperty("browser");
+            WebDriverManager.chromedriver().setup();
 
-            switch (browser.toLowerCase()) {
-                case "chrome":
-                default:
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
-                    break;
-            }
+            ChromeOptions options = new ChromeOptions();
+
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+
+            driver = new ChromeDriver(options);
 
             driver.manage().window().maximize();
         }
