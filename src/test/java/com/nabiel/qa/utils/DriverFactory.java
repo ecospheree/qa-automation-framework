e4eb90d76a436a93c328c8dfa55c717846ebead2
ecospheree <prayoganabiel74@gmail.com>
2026-07-12 18:36:22 +0700

    refactor: use Selenium Manager for ChromeDriver

--- src/test/java/com/nabiel/qa/utils/DriverFactory.java
+++ src/test/java/com/nabiel/qa/utils/DriverFactory.java
@@ -13,14 +13,11 @@ public class DriverFactory {
 
         if (driver == null) {
 
-            WebDriverManager.chromedriver().setup();
-
             ChromeOptions options = new ChromeOptions();
+
             options.addArguments("--headless=new");
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--disable-gpu");
-            options.addArguments("--remote-allow-origins=*");
             options.addArguments("--window-size=1920,1080");
 
             driver = new ChromeDriver(options);
