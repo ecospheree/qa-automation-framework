1f71ea2ce6ab94fcb19c20977e3e1a5a28b837b0
ecospheree <prayoganabiel74@gmail.com>
2026-07-12 18:03:38 +0700

    Add CI pipeline and project documentation

--- src/test/java/com/nabiel/qa/utils/DriverFactory.java
+++ src/test/java/com/nabiel/qa/utils/DriverFactory.java
@@ -16,11 +16,11 @@ public class DriverFactory {
             WebDriverManager.chromedriver().setup();
 
             ChromeOptions options = new ChromeOptions();
-
             options.addArguments("--headless=new");
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
             options.addArguments("--disable-gpu");
+            options.addArguments("--remote-allow-origins=*");
             options.addArguments("--window-size=1920,1080");
 
             driver = new ChromeDriver(options);
